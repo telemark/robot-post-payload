960598264967930f60af1b3133a8e7deeab086fd
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-08-10 08:47:33 +0200

    Adds handling of retries in runner (minor)

--- runner.js
+++ runner.js
@@ -1,10 +1,20 @@
+const { readdir } = require('fs').promises
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
-const getNextJobFromQueue = require('./lib/steps/get-next-job-from-queue')
+const config = require('./config')
+const isJson = file => file.endsWith('.json')
+
+async function gotJobs () {
+  const allRetryFiles = await readdir(`${config.RETRY_DIRECTORY_PATH}`)
+  const allJobFiles = await readdir(`${config.QUEUE_DIRECTORY_PATH}`)
+  const retryFiles = allRetryFiles.filter(isJson)
+  const jobFiles = allJobFiles.filter(isJson)
+  return retryFiles.length + jobFiles.length > 0
+}
 
 async function checkQueue () {
-  const job = await getNextJobFromQueue()
-  if (job) {
+  const job = await gotJobs()
+  if (job === true) {
     console.log('Here we go')
     const cmd = `node index.js`
     const { stdout, stderr } = await exec(cmd)
